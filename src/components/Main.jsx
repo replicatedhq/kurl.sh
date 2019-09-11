a586703fa16e837d9f474946b352b5153a8cc305
Jelena <jelena.gruica@gmail.com>
2019-09-11 18:05:15 +0000

    Ui updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import autoBind from "react-autobind";
 import { withRouter } from "react-router-dom";
 
 import MonacoEditor from "react-monaco-editor";
+import Select from "react-select";
 
 import "../scss/components/Main";
 
@@ -10,6 +11,10 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      kubernetesVersions: [
+        {version: "0.15.0"}
+      ],
+      selectedVersion: 0,
       kubernetesYaml: `apiVersion: kurl.sh/v1beta1
       kind: Installer
       metadata:
@@ -28,6 +33,10 @@ class Main extends React.Component {
     autoBind(this);
   }
 
+  onVersionChange = (selectedVersion) => {
+    this.setState({ selectedVersion });
+  }
+
   render() {
     return (
       <div className="u-minHeight--full u-width--full u-overflow--auto container flex-column flex1 alignItems--center">
@@ -37,11 +46,62 @@ class Main extends React.Component {
         </div>
         <div className="u-flexTabletReflow flex-1-auto u-width--full">
           <div className="flex1 flex-column">
-            <div className="left-content-wrap flex-column justifyContent--center alignItems--center">
+            <div className="left-content-wrap flex-column">
               <div className="u-marginTop--more u-fontSize--larger u-fontWeight--medium u-lineHeight--more u-color--tuna">
-                Kurl is a Kubernetes installer for airgapped and online clusters. 
-                This form allows you to quickly build an installer and will provide you with a URL that it can be installed at.
+                  Kurl is a Kubernetes installer for airgapped and online clusters. 
+                  This form allows you to quickly build an installer and will provide you with a URL that it can be installed at.
+                </div>
+                <div className="flex u-marginTop--30">
+                  <div className="flex flex1">
+                    <div className="flex1"> 
+                      <div className="FormLabel"> Kubernetes version </div>
+                      <div className="u-fontWeight--normal u-color--dustyGray u-lineHeight--normal u-marginBottom--more"> What version of Kubernetes are you using? </div>
+                    </div>  
+                    <div className="flex1 u-paddingLeft--60 alignSelf--center"> 
+                      <div className="SelectApp--wrapper">
+                        <Select
+                          options={this.state.kubernetesVersions}
+                          getOptionLabel={this.getLabel}
+                          getOptionValue={(kubernetes) => kubernetes.verison}
+                          value={this.state.selectedVersion}
+                          onChange={this.onVersionChange}
+                          matchProp="value"
+                          isOptionSelected={() => false}
+                        />
+                      </div>
+                    </div>  
                   </div>
+                </div>
+
+                <div className="flex u-marginTop--30">
+                  <div className="flex flex1">
+                    <div className="flex1"> 
+                      <div className="FormLabel"> Weave version </div>
+                      <div className="u-fontWeight--normal u-color--dustyGray u-lineHeight--normal u-marginBottom--more"> What version of Weave are you using? </div>
+                    </div>  
+                    <div className="flex1 u-paddingLeft--60 alignSelf--center"> 2</div>  
+                  </div>
+                </div>
+
+                <div className="flex u-marginTop--30">
+                  <div className="flex flex1">
+                    <div className="flex1"> 
+                      <div className="FormLabel"> Contour version </div>
+                      <div className="u-fontWeight--normal u-color--dustyGray u-lineHeight--normal u-marginBottom--more"> What version of Contour are you using? </div>
+                    </div>  
+                    <div className="flex1 u-paddingLeft--60 alignSelf--center"> 2</div>  
+                  </div>
+                </div>
+
+                <div className="flex u-marginTop--30">
+                  <div className="flex flex1">
+                    <div className="flex1"> 
+                      <div className="FormLabel"> Rook version </div>
+                      <div className="u-fontWeight--normal u-color--dustyGray u-lineHeight--normal u-marginBottom--more"> What version of Rook are you using? </div>
+                    </div>  
+                    <div className="flex1 u-paddingLeft--60 alignSelf--center"> 2</div>  
+                  </div>
+                </div>
               </div>
             </div>
           <div className="u-paddingLeft--60 flex-1-auto flex-column">
