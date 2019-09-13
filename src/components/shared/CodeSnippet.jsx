8b2fba0407f8deed9f9355f9b17350e447447507
Jelena <jelena.gruica@gmail.com>
2019-09-13 14:14:34 +0000

    Removed unused prism

--- src/components/shared/CodeSnippet.jsx
+++ src/components/shared/CodeSnippet.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import classNames from "classnames";
 import PropTypes from "prop-types";
-import Prism from "@maji/react-prism";
 
 import "../../scss/components/shared/CodeSnippet.scss";
 
