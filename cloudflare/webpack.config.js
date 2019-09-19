1ff3c8259ffd30f1655b48df299831904a0626d4
Marc Campbell <marc.e.campbell@gmail.com>
2019-09-19 00:47:23 +0000

    Creating a better dev env

--- cloudflare/webpack.config.js
+++ cloudflare/webpack.config.js
@@ -3,6 +3,20 @@ const webpack = require('webpack');
 const ReplacePlugin = require("webpack-plugin-replace");
 const appEnv = process.env.KURL_ENV;
 
+var backend;
+var frontend;
+
+if (appEnv === "dev") {
+  backend = "https://backend.staging.kurl.sh";
+  frontend = "https://kurlsh-staging.netlify.com";
+} else if (appEnv === "staging") {
+  backend = "https://backend.staging.kurl.sh";
+  frontend = "https://kurlsh-staging.netlify.com";
+} else if (appEnv === "prod") {
+  backend = "https://backend.kurl.sh";
+  frontend = "https://kurlsh.netlify.com";
+}
+
 module.exports = {
   entry: {
     bundle: path.join(__dirname, "./src/index.ts"),
@@ -21,8 +35,8 @@ module.exports = {
         /node_modules/,
       ],
       values: {
-        '__BACKEND__': appEnv === "staging" ? "https://backend.staging.kurl.sh" : "https://backend.kurl.sh",
-        '__FRONTEND__': appEnv === "staging" ? "https://kurlsh-staging.netlify.com" : "https://kurlsh.netlify.com",
+        '__BACKEND__': backend,
+        '__FRONTEND__': frontend,
       },
     }),
   ],
