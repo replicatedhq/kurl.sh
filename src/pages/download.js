6c36b79310011c9be0864ee0d3e82b4d487d12da
Jelena <jelena.gruica@gmail.com>
2019-09-30 14:26:33 +0000

    Refactored - added layout component

--- src/pages/download.js
+++ src/pages/download.js
@@ -1,22 +1,14 @@
 import React from "react"
-import { Router, Location } from "@reach/router"
-import NavBar from "../components/shared/NavBar";
+import { Router, Location } from "@reach/router";
+import Layout from "../components/Layout";
 import DownloadAirgapBundle from "../components/DownloadAirgapBundle";
-import Footer from "../components/shared/Footer"
 
 const Download = () => (
-  <div className="u-minHeight--full flex-column flex1">
-    <div className="flex-column flex1">
-      <NavBar />
-
-      <FadeTransitionRouter>
-        <DownloadAirgapBundle path="download/:sha" />
-      </FadeTransitionRouter>
-      <div className="flex-auto Footer-wrapper u-width--full">
-        <Footer />
-      </div>
-    </div>
-  </div>
+  <Layout>
+    <FadeTransitionRouter>
+      <DownloadAirgapBundle path="download/:sha" />
+    </FadeTransitionRouter>
+  </Layout>
 )
 
 const FadeTransitionRouter = props => (
