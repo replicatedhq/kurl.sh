a76033605e719bc6c9aa9152f6d6b44dea984360
Jelena <jelena.gruica@gmail.com>
2019-10-01 11:09:31 +0000

    Removing cursor and border radius on supported add ons

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/jelena/go/src/github.com/replicatedhq/kurl.sh/.cache/dev-404-page.js"))),
   "component---src-pages-add-ons-js": hot(preferDefault(require("/home/jelena/go/src/github.com/replicatedhq/kurl.sh/src/pages/add-ons.js"))),
   "component---src-pages-download-js": hot(preferDefault(require("/home/jelena/go/src/github.com/replicatedhq/kurl.sh/src/pages/download.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/jelena/go/src/github.com/replicatedhq/kurl.sh/src/pages/index.js")))
