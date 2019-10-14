e9bace68a392d9308985ea0ef81dd2123be4b542
Jelena <jelena.gruica@gmail.com>
2019-10-14 16:36:16 +0000

    Small updates on kurl text

--- src/components/Kurlsh.js
+++ src/components/Kurlsh.js
@@ -260,12 +260,13 @@ spec:
     return (
       <div className="u-minHeight--full u-width--full u-overflow--auto container flex-column flex1 u-marginBottom---40">
         <div className="u-flexTabletReflow flex1 u-width--full">
-          <div className="flex">
+          <div className="flex u-width--70">
             <div className="left-content-wrap flex-column">
               <div className="u-marginTop--more u-fontSize--larger u-fontWeight--medium u-lineHeight--more u-color--tuna">
-                Kurl is a custom Kubernetes distro creator. Like a link shortener for your favorite Kubernetes base components (aka add-ons), it creates a unique URL for your specific distro that can be installed with curl on a modern Linux server (both online and airgapped).
-                Kurl is completely open source and easily extensible by contributing additional add-ons as Kustomization overlays on common components.
-                </div>
+                <p> Kurl is a custom Kubernetes distro creator that uses Kustomize to package add-ons into the cluster installer. </p>
+                <p> Kurl is a link shortener for your favorite Kubernetes base components (aka add-ons). It creates a unique URL for your specific components that can be installed with curl on a modern Linux server. Kurl installation packages can be run online or download and executed in a completely airgapped environment. </p>
+                <p> Kurl is open source and easily extensible by contributing additional add-ons as Kustomization overlays.</p>
+              </div>
               <div className="flex u-marginTop--30">
                 <div className="flex-column flex flex1">
                   <div className="flex flex1">
