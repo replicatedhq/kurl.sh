e13fdea7c3a40165ca71273ed6fe88281eecb5d1
Ben Junya <ben@replicated.com>
2019-10-31 14:08:46 -0700

    Fix header on docspage navbar

--- src/components/shared/DocumentationLayout.js
+++ src/components/shared/DocumentationLayout.js
@@ -2,12 +2,51 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
+import { BreakpointConfig } from "../../services/breakpoints";
+
 import Sidebar from "../Sidebar";
 import Navbar from "./NavBar";
 
 export default class DocumentationLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: false
+    };
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.onResize);
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.onResize);
+    this.onResize();
+  }
+
+  onResize = () => {
+    const { isMobile } = this.state;
+    const width = window.innerWidth;
+    const { breakpoints } = BreakpointConfig;
+
+    if (isMobile && width > breakpoints.desktopSmall) {
+      this.setState({
+        isMobile: false
+      });
+      return;
+    }
+
+    if (!isMobile && width < breakpoints.desktopSmall) {
+      this.setState({
+        isMobile: true
+      });
+
+    }
+  }
+
   render() {
-    const { children, isMobile } = this.props;
+    const { children } = this.props;
+    const { isMobile } = this.state;
 
     return (
       <StaticQuery
