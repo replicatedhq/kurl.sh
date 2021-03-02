3aefbbf15127bab8c3962bd5399cc359e91c49bf
emosbaugh <ethan@replicated.com>
2021-03-02 12:38:34 -0800

    Removing, rebooting and resetting node docs (#355)
    * Removing, rebooting and resetting node docs
    
    * Anchor link margin fix
    
    * reorder
    
    * Update src/markdown-pages/install-with-kurl/adding-nodes.md
    
    Co-authored-by: Andrew Reed <areed@users.noreply.github.com>
    
    * Update adding-nodes.md
    
    * cluster health
    
    * Update src/markdown-pages/install-with-kurl/adding-nodes.md
    
    Co-authored-by: Andrew Reed <areed@users.noreply.github.com>
    
    * Update src/markdown-pages/install-with-kurl/adding-nodes.md
    
    Co-authored-by: Andrew Reed <areed@users.noreply.github.com>
    
    Co-authored-by: jelena <jelena.gruica@gmail.com>
    Co-authored-by: Andrew Reed <areed@users.noreply.github.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,7 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          `gatsby-remark-autolink-headers`,
           {
             resolve: 'gatsby-remark-mermaid',
             options: {
