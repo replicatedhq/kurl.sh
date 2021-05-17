7473556d5a9d4150a114cbd74ffbf9621f7f7ef0
emosbaugh <ethan@replicated.com>
2021-05-17 15:14:44 -0700

    Release notes (#410)
    * Release notes
    
    * Sidebar link to index page
    
    * Proper sorting
    
    * keeping up-to-date with release notes
    
    * dedicated primary nodes
    
    * Rearrage releases
    
    * fixes
    
    * style cleanup
    
    * Hide anchors from release notes
    
    Co-authored-by: jelena <jelena.gruica@gmail.com>

--- src/components/SidebarReleaseNotes.js
+++ src/components/SidebarReleaseNotes.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import classNames from "classnames";
-import { StaticQuery, graphql } from "gatsby";
+import { StaticQuery, graphql, Link } from "gatsby";
 import SidebarFileTree from "./shared/SidebarFileTree";
 import { parseLinksToTree } from "../utils/parse-links-to-tree";
 
@@ -13,17 +13,13 @@ export default class Sidebar extends Component {
         query={graphql`
       {
         allMarkdownRemark(
-          filter: { fields: { collection: { eq: "markdown-pages" } } }
-          sort: { fields: [frontmatter___weight], order: ASC }
+          filter: { fields: { collection: { eq: "release-notes" } } }
+          sort: { fields: [frontmatter___weight], order: DESC }
         ) {
           edges {
             node {
               frontmatter {
-                path
-                linktitle
-                title
-                isAlpha
-                isBeta
+                version
               }
             }
           }
@@ -31,16 +27,24 @@ export default class Sidebar extends Component {
       }
     `}
         render={({ allMarkdownRemark: { edges: pages } }) => {
-          const tree = parseLinksToTree(pages);
+          const releaseNotesPages = pages.map(page => {
+            page.node.frontmatter.path = `/release-notes/${page.node.frontmatter.version}`;
+            page.node.frontmatter.linktitle = page.node.frontmatter.version;
+            return page;
+          });
+          const tree = parseLinksToTree(releaseNotesPages);
           return (
             <div className={classNames("flex-column flex1", {
               "Sidebar": !this.props.isMobile,
-
             })}>
               <div className={`${this.props.isMobile ? "u-paddingBottom--20" : "Sidebar-content u-position--relative"}`}>
+                <h3 className="u-paddingLeft--20 u-margin-none">
+                  <Link to="/release-notes">Release Notes</Link>
+                </h3>
                 <SidebarFileTree
                   data={tree}
                   pathname={this.props.pathname}
+                  isReleaseNotes={true}
                 />
               </div>
             </div>
---
+++ src/templates/ReleaseNotesListTemplate.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+import ReleaseNotesLayout from "../components/shared/ReleaseNotesLayout";
+import Post from "../components/shared/ReleaseNotesPost"
+
+class ReleaseNotesIndex extends React.Component {
+  render() {
+    const { data } = this.props;
+    const edges = data.allMarkdownRemark.edges;
+    const { currentPage, numPages } = this.props.pageContext;
+    const isFirst = currentPage === 1;
+    const isLast = currentPage === numPages;
+    const prevPage = currentPage - 1 === 1 ? '/release-notes' : `/release-notes/${currentPage - 1}`;
+    const nextPage = `/release-notes/${currentPage + 1}`;
+
+    const Posts = edges
+      .map((edge, i) => <Post key={`${edge.node.frontmatter.title}-${i}`} post={edge.node} link={true} />);
+
+    return (
+      <ReleaseNotesLayout location={{pathname: "/release-notes"}} title="Release Notes">
+        {Posts}
+        <div className="paginator">
+        <ul
+          className="pagination"
+        >
+          {!isFirst && (
+            <Link to={prevPage} rel="prev">
+              <span className="icon u-backIcon" />
+            </Link>
+          )}
+          {Array.from({ length: numPages }, (_, i) => (
+            <li
+              key={`pagination-number${i + 1}`}
+            >
+              <Link
+                to={i === 0 ? `/release-notes` : `/release-notes/${i + 1}`}
+                style={{
+                  padding: .25,
+                  textDecoration: "none",
+                  color: i + 1 === currentPage ? "#4A4A4A" : "#326DE6",
+                }}
+              >
+                {i + 1}
+              </Link>
+            </li>
+          ))}
+          {!isLast && (
+            <Link to={nextPage} rel="next">
+              <span className="icon u-forwardIcon" /> 
+            </Link>
+          )}
+        </ul>
+        </div>
+      </ReleaseNotesLayout>
+    );
+  }
+};
+
+export default ReleaseNotesIndex;
+
+export const pageQuery = graphql`
+  query($skip: Int!, $limit: Int!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      filter: { fields: { collection: { eq: "release-notes" } } }
+      sort: { fields: [frontmatter___weight], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          html
+          fields {
+            collection
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            version
+          }
+        }
+      }
+    }
+  }
+`;
