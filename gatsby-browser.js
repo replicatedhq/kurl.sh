209bd4b2f8110e7d2bf478a3d29c8aa7080e1ceb
Jalaja <jalaja@replicated.com>
2022-09-09 00:28:44 +0000

    Set the Release stages and send notifications only to prod and staging

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,10 @@ const Bugsnag = require('@bugsnag/js').default
 const BugsnagPluginReact = require('@bugsnag/plugin-react').default
 
 Bugsnag.start({
-  apiKey: `${process.env.BUGSNAG_API_KEY}`,
-  plugins: [new BugsnagPluginReact()],
+    apiKey: `${process.env.BUGSNAG_API_KEY}`,
+    releaseStage: `${process.env.BUGSNAG_RELEASE_STAGE}`,
+    notifyReleaseStages: ["production", "staging"],
+    plugins: [new BugsnagPluginReact()],
 })
 
 const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
