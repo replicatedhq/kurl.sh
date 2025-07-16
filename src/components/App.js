38908395a802cf6b48ca9acc5b4be511de9dbec8
Gerard Nguyen <gerard@replicated.com>
2025-07-16 10:58:47 +1000

    support RHEL and Rocky 9.6

--- src/components/App.js
+++ src/components/App.js
@@ -197,10 +197,10 @@ cat install.sh | sudo bash -s airgap
                       CentOS 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.1, 8.2, 8.3, 8.4 (CentOS 8.x requires Containerd)
                     </li>
                     <li className="u-fontSize--small u-color--dustyGray u-fontWeight--medium u-lineHeight--normal">
-                      RHEL 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 8.10, 9.0, 9.1, 9.2, 9.3, 9.4, 9.5 (RHEL 8.x and 9.x require Containerd)
+                      RHEL 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 8.10, 9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6 (RHEL 8.x and 9.x require Containerd)
                     </li>
                     <li className="u-fontSize--small u-color--dustyGray u-fontWeight--medium u-lineHeight--normal">
-                      Rocky Linux 9.0, 9.1, 9.2, 9.3, 9.4, 9.5 (Rocky Linux 9.x requires Containerd {'>'}= 1.6.28)
+                      Rocky Linux 9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6 (Rocky Linux 9.x requires Containerd {'>'}= 1.6.28)
                     </li>
                     <li className="u-fontSize--small u-color--dustyGray u-fontWeight--medium u-lineHeight--normal">
                       Oracle Linux 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 8.10 (OL 8.x requires Containerd)
