0ce9da04521833601d4236e8e8aab3eca9cf48dd
Ethan Mosbaugh <ethan@replicated.com>
2025-08-05 10:50:57 -0700

    chore(deps): remove unused deps (#1151)
    * chore(deps): remove unused deps
    
    * f
    
    * f

--- src/components/SupportedAddOns.js
+++ src/components/SupportedAddOns.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as _ from "lodash";
+import _ from "lodash";
 import Select from "react-select";
 import { Link, navigate } from "gatsby";
 import MobileCategories from "./MobileCategories";
